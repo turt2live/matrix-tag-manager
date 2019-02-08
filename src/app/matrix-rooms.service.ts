27690096ad12b4fc6f712f688445430f51654562
Travis Ralston <travpc@gmail.com>
2019-02-07 23:54:56 -0700

    Show the user's real tags

--- src/app/matrix-rooms.service.ts
+++ src/app/matrix-rooms.service.ts
@@ -4,6 +4,10 @@ import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 
+export interface IRoomTags {
+    [tagName: string]: { order: number, roomId: string }[];
+}
+
 @Injectable({
     providedIn: 'root'
 })
@@ -36,5 +40,54 @@ export class MatrixRoomsService {
                 "Authorization": `Bearer ${this.accessToken}`,
             },
         }).pipe(this.auth.logoutIfUnauthorized());
+    }
+
+    public getTags(): Observable<IRoomTags> {
+        const filter = {
+            event_fields: ['content.tags'],
+            presence: {types: [], limit: 0},
+            account_data: {types: [], limit: 0},
+            room: {
+                ephemeral: {types: [], limit: 0},
+                state: {types: [], limit: 0},
+                timeline: {types: [], limit: 0},
+                account_data: {types: ['m.tag']},
+            },
+        };
+        const encodedFilter = encodeURIComponent(JSON.stringify(filter));
+        return this.http.get(`${this.homeserverUrl}/_matrix/client/r0/sync?filter=${encodedFilter}`, {
+            headers: {
+                "Authorization": `Bearer ${this.accessToken}`,
+            },
+        }).pipe(this.auth.logoutIfUnauthorized(), map(r => {
+            if (!r['rooms'] || !r['rooms']['join']) return {};
+
+            const resultingTags = {};
+
+            const joinedRooms = r['rooms']['join'];
+            for (const roomId of Object.keys(joinedRooms)) {
+                const accountData = joinedRooms[roomId]['account_data'];
+                if (!accountData || !accountData['events']) continue;
+                for (const event of accountData['events']) {
+                    if (event['type'] !== 'm.tag') continue;
+
+                    const content = event['content'];
+                    if (!content || !content['tags']) continue;
+
+                    for (const tagName of Object.keys(content['tags'])) {
+                        const tag = content['tags'][tagName];
+                        if (!resultingTags[tagName]) resultingTags[tagName] = [];
+                        resultingTags[tagName].push({order: tag['order'] || 1, roomId: roomId});
+                    }
+                }
+            }
+
+            // Sort the tags by order
+            for (const tagName of Object.keys(resultingTags)) {
+                resultingTags[tagName].sort((a, b) => b.order - a.order);
+            }
+
+            return resultingTags;
+        }));
     }
 }
--- src/app/tag-manager/tag-manager.component.ts
+++ src/app/tag-manager/tag-manager.component.ts
@@ -11,12 +11,12 @@ export class TagManagerComponent implements OnInit {
 
     public userId: string;
     public loadingRooms = true;
-    public joinedRooms: string[] = [];
     public newTagName: string;
 
     public tags: { [tagName: string]: string[] } = {
+        "Favourites": [],
         "Default": [],
-        "Test Tag": [],
+        "Low Priority": [],
     };
 
     public get tagNames(): string[] {
@@ -32,8 +32,24 @@ export class TagManagerComponent implements OnInit {
         });
         this.rooms.getJoinedRooms().subscribe(joinedRooms => {
             this.loadingRooms = false;
-            this.joinedRooms = joinedRooms;
-            this.tags["Default"] = this.joinedRooms;
+
+            this.rooms.getTags().subscribe(tags => {
+                const sortedRooms = [];
+                for (const tagName of Object.keys(tags)) {
+                    let adjustedTagName = tagName;
+                    if (tagName === "m.favourite") adjustedTagName = "Favourites";
+                    if (tagName === "m.lowpriority") adjustedTagName = "Low Priority";
+
+                    if (!this.tags[adjustedTagName]) this.tags[adjustedTagName] = [];
+                    tags[tagName].map(room => {
+                        this.tags[adjustedTagName].push(room.roomId);
+                        sortedRooms.push(room.roomId);
+                    });
+                }
+                joinedRooms.filter(rid => !sortedRooms.includes(rid)).map(rid => {
+                    this.tags["Default"].push(rid);
+                });
+            });
         });
     }
 
