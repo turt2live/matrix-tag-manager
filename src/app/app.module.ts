fea0a6e0090e35b593d035219d88eb84f1472e8e
Travis Ralston <travpc@gmail.com>
2019-02-07 23:32:59 -0700

    Drag and drop of room tiles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,20 @@ import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 import { TagManagerComponent } from './tag-manager/tag-manager.component';
 import { RoomTileComponent } from './room-tile/room-tile.component';
+import { DragulaModule } from "ng2-dragula";
 
 @NgModule({
     declarations: [
         AppComponent,
         LoginFormComponent,
         TagManagerComponent,
-        RoomTileComponent
+        RoomTileComponent,
     ],
     imports: [
         BrowserModule,
         HttpClientModule,
         FormsModule,
+        DragulaModule.forRoot(),
     ],
     providers: [],
     bootstrap: [AppComponent]
--- src/app/tag-manager/tag-manager.component.ts
+++ src/app/tag-manager/tag-manager.component.ts
@@ -12,6 +12,16 @@ export class TagManagerComponent implements OnInit {
     public userId: string;
     public loadingRooms = true;
     public joinedRooms: string[] = [];
+    public newTagName: string;
+
+    public tags: { [tagName: string]: string[] } = {
+        "Default": [],
+        "Test Tag": [],
+    };
+
+    public get tagNames(): string[] {
+        return Object.keys(this.tags);
+    }
 
     constructor(private auth: MatrixAuthService, private rooms: MatrixRoomsService) {
     }
@@ -23,10 +33,20 @@ export class TagManagerComponent implements OnInit {
         this.rooms.getJoinedRooms().subscribe(joinedRooms => {
             this.loadingRooms = false;
             this.joinedRooms = joinedRooms;
+            this.tags["Default"] = this.joinedRooms;
         });
     }
 
     public logout() {
         this.auth.logout();
+    }
+
+    public setTaggedRooms(tagName, roomIds) {
+        console.log("Set " + tagName + " to : " + JSON.stringify(roomIds));
+    }
+
+    public addTag() {
+        this.tags[this.newTagName] = [];
+        this.newTagName = "";
     }
 }
